0404894bf7ac39a8df1ec185d06edd250e793cae
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-06-16 14:14:39 +0100

    initial commit

---
+++ src/EstateApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EstateApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
